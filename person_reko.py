6be098d6a64aeec5a82f84670f4dc0a8cbbf455a
Juan2040751 <viafara.juan@correounivalle.edu.co>
2024-03-06 10:29:19 -0500

    Union CDAI-4 y CDAI-7

--- person_reko.py
+++ person_reko.py
@@ -1,11 +1,8 @@
 import boto3
 
 
-def contar_personas(imagen_path, perfil_aws='RekognitionFullAccess_user'):
+def contar_personas(imagen_binaria, perfil_aws='RekognitionFullAccess_user'):
     rekognition_client = boto3.Session(profile_name=perfil_aws).client('rekognition')
-
-    with open(imagen_path, 'rb') as imagen_archivo:
-        imagen_binaria = imagen_archivo.read()
 
     try:
         response = rekognition_client.detect_faces(
@@ -17,10 +14,9 @@ def contar_personas(imagen_path, perfil_aws='RekognitionFullAccess_user'):
         return numero_personas
 
     except Exception as e:
-        print(f"Error al analizar la imagen: {e}")
-        return 0
+        return f"Error al analizar la imagen: {e}"
 
 
-ruta_imagen = 'p1.jpg'
-cantidad_personas = contar_personas(ruta_imagen)
+#ruta_imagen = 'p1.jpg'
+#cantidad_personas = contar_personas(ruta_imagen)
 # print(cantidad_personas)
