a6bed33e3e2764b0269722d6b862fded54f5d20a
Juan2040751 <viafara.juan@correounivalle.edu.co>
2024-03-08 20:22:50 -0500

    CDAI-4 blob

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from json import loads
 
-import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from person_reko import contar_personas
-
+from PIL import Image
+import io
 app = Flask(__name__)
+CORS(app)
 
 # Endpoint para recibir las tres imágenes y devolver el promedio de las respuestas
-@app.route('/', methods=['POST'])
+@app.route('/',methods=['POST', "post", 'OPTIONS'])
 def procesar_imagenes():
     try:
-        data = loads(request.data.decode().replace("'", '"'))
+        respuestas = []
+        for i in range(1, 4):
+            blob = request.files.get(f'imagen{i}')
+            imagen_bytes = blob.read()
+            respuestas.append(contar_personas(imagen_bytes))
 
 
-        imagen1_bytes, imagen2_bytes, imagen3_bytes = data["image1"], data["image2"], data["image3"]
-        
-        respuestas = [contar_personas(imagen1_bytes), contar_personas(imagen2_bytes), contar_personas(imagen3_bytes)]
-    
-        promedio_respuestas = sum(respuestas) / 3.0
 
-        return jsonify({'promedio_respuestas': promedio_respuestas}), 200
+        #print(imagenes_bytes)
+        #respuestas = [contar_personas(imagen_bytes) for imagen_bytes in imagenes_bytes.values()]
+        #promedio_respuestas = sum(respuestas) / 3.0
+
+        return jsonify({'promedio_respuestas': 100}), 200
     except Exception as e:
-        return jsonify({str(e)}), 500
+        print(e)
+        return jsonify({"error":str(e)}), 500
 
 
 if __name__ == '__main__':
